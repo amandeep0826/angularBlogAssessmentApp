da171af2521cd71a02b7b8096ef062029818aeb0
Amandeep <amandeep.henceforth@gmail.com>
2021-11-08 12:38:13 +0530

    Components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ViewBlogComponent } from './view-blog/view-blog.component';
+import { CreateBlogComponent } from './create-blog/create-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactUsComponent,
+    ViewBlogComponent,
+    CreateBlogComponent
   ],
   imports: [
     BrowserModule,
