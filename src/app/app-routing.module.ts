a154986f93108583c3c5acd83bd80d0f39b4b030
Amandeep <amandeep.henceforth@gmail.com>
2021-11-10 19:11:19 +0530

    route for contact page and banner style fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
 import { CreateBlogComponent } from './create-blog/create-blog.component';
 import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'create-blog', component: CreateBlogComponent }
+  { path: 'create-blog', component: CreateBlogComponent },
+  { path: 'contact-us', component: ContactUsComponent }
 ];
 
 @NgModule({
