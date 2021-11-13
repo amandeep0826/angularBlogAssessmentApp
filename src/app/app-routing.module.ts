6746ec47a27563c5e8171a6c550ec40b5f96c9a7
Amandeep <amandeep.henceforth@gmail.com>
2021-11-13 14:53:40 +0530

    view blog page design implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http'
 import { CreateBlogComponent } from './create-blog/create-blog.component';
 import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ViewBlogComponent } from './view-blog/view-blog.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'create-blog', component: CreateBlogComponent },
-  { path: 'contact-us', component: ContactUsComponent }
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'blog-card/:blogId', component: ViewBlogComponent }
 ];
 
 @NgModule({
