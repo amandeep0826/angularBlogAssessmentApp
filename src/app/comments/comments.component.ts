97830d9d01049525210b3e5608bc5e4c0cbef382
Amandeep <amandeep.henceforth@gmail.com>
2021-12-03 16:36:37 +0530

    delete comment feature added

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 
@@ -10,59 +10,62 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 })
 export class CommentsComponent implements OnInit {
 
-  comments: any
+  @Input() commentData: any
+
+  // comments: any
   commentId: any
   commentIdInt: any
   constructor(private http: HttpClient, private route: ActivatedRoute, private loader: NgxUiLoaderService) {
 
-    this.commentId = this.route.snapshot.params.blogId
+    // this.commentId = this.route.snapshot.params.blogId
     this.commentIdInt = Number(this.route.snapshot.params.blogId)
-    var url = "http://localhost:3000/comments?postId=" + this.commentId
-    this.http.get(url).subscribe((response: any) => {
-      console.log("Response from comments api", response)
-      this.comments = response
-      this.loader.stop()
-    }, (error) => {
-      console.log("Error from comments api", error)
-      this.loader.stop()
-    })
+    // var url = "http://localhost:3000/comments?postId=" + this.commentId
+    // this.http.get(url).subscribe((response: any) => {
+    //   console.log("Response from comments api", response)
+    //   this.comments = response
+    //   this.loader.stop()
+    // }, (error) => {
+    //   console.log("Error from comments api", error)
+    //   this.loader.stop()
+    // })
   }
 
   ngOnInit(): void {
   }
 
-  commentText: any
-  commentatorName: any
+  // commentText: any
+  // commentatorName: any
 
-  postComment() {
-    var requestJson = {
-      body: this.commentText,
-      author: this.commentatorName,
-      postId: this.commentIdInt
-    }
-    var url = "http://localhost:3000/comments/"
-    this.loader.start()
-    this.http.post(url, requestJson).subscribe((response: any) => {
-      location.reload();
-      this.loader.stop();
-      console.log("Response from post comment api", response)
-    }, (error) => {
-      location.reload();
-      this.loader.stop();
-      console.log("Error from post comment api", error)
-    })
-  }
-
-  // deleteComment() {
-  //   var url = "http://localhost:3000/comments" + this.commentId
-  //   this.http.delete(url).subscribe((response: any) => {
+  // postComment() {
+  //   var requestJson = {
+  //     body: this.commentText,
+  //     author: this.commentatorName,
+  //     postId: this.commentIdInt
+  //   }
+  //   var url = "http://localhost:3000/comments/"
+  //   this.loader.start()
+  //   this.http.post(url, requestJson).subscribe((response: any) => {
   //     location.reload();
   //     this.loader.stop();
-  //     console.log("Response from delete comment api", response)
+  //     console.log("Response from post comment api", response)
   //   }, (error) => {
+  //     location.reload();
   //     this.loader.stop();
-  //     console.log("Error from delete comment api", error)
+  //     console.log("Error from post comment api", error)
   //   })
   // }
+
+  deleteComment(commentId: any) {
+    var url = "http://localhost:3000/comments/" + commentId
+    console.log("url", url)
+    this.http.delete(url).subscribe((response: any) => {
+      location.reload();
+      this.loader.stop();
+      console.log("Response from delete comment api", response)
+    }, (error) => {
+      this.loader.stop();
+      console.log("Error from delete comment api", error)
+    })
+  }
 
 }
--- src/app/view-blog/view-blog.component.ts
+++ src/app/view-blog/view-blog.component.ts
@@ -11,6 +11,10 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewBlogComponent implements OnInit {
   blogDetail: any
 
+  comments: any
+  commentId: any
+  commentIdInt: any
+
   constructor(private http: HttpClient, private route: ActivatedRoute, private loader: NgxUiLoaderService) {
 
     var blogId = this.route.snapshot.params.blogId
@@ -25,9 +29,57 @@ export class ViewBlogComponent implements OnInit {
       this.loader.stop()
     })
 
+
+    this.commentId = this.route.snapshot.params.blogId
+    this.commentIdInt = Number(this.route.snapshot.params.blogId)
+    var url = "http://localhost:3000/comments?postId=" + this.commentId
+    this.http.get(url).subscribe((response: any) => {
+      console.log("Response from comments api", response)
+      this.comments = response
+      this.loader.stop()
+    }, (error) => {
+      console.log("Error from comments api", error)
+      this.loader.stop()
+    })
+
   }
 
   ngOnInit(): void {
+  }
+
+  commentText: any
+  commentatorName: any
+
+  postComment() {
+    var requestJson = {
+      body: this.commentText,
+      author: this.commentatorName,
+      postId: this.commentIdInt
+    }
+    var url = "http://localhost:3000/comments/"
+    this.loader.start()
+    this.http.post(url, requestJson).subscribe((response: any) => {
+      location.reload();
+      this.loader.stop();
+      console.log("Response from post comment api", response)
+    }, (error) => {
+      location.reload();
+      this.loader.stop();
+      console.log("Error from post comment api", error)
+    })
+  }
+
+  deleteComment() {
+    var url = "http://localhost:3000/comments" + this.commentId
+    console.log("comment url", url)
+    // this.http.delete(url).subscribe((response: any) => {
+    //   location.reload();
+    //   this.loader.stop();
+    //   console.log("Response from delete comment api", response)
+    // }, (error) => {
+    //   this.loader.stop();
+    //   console.log("Error from delete comment api", error)
+    // })
   }
 
 }
