92f3efaf49c6741f15a1f99b5dd2d8a1c07c5af5
Amandeep <amandeep.henceforth@gmail.com>
2021-12-04 14:27:51 +0530

    create blog feature added

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-create-blog',
@@ -7,7 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBlogComponent implements OnInit {
 
-  constructor() { }
+  blogTitle: any
+  blogAuthor: any
+  blogAuthorInfo: any
+  blogCategory: any
+  blogDate: any
+  blogHashtags: any
+  blogContent: any
+  blogIdCreateBlog: any
+
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private loader: NgxUiLoaderService) {
+  }
+
+  postBlog() {
+    this.loader.start()
+    var url = "http://localhost:3000/posts"
+    var requestJson = {
+      title: this.blogTitle,
+      author: this.blogAuthor,
+      authorInfo: this.blogAuthorInfo,
+      category: this.blogCategory,
+      date: this.blogDate,
+      hashtags: this.blogHashtags,
+      content: this.blogContent
+    }
+    this.http.post(url, requestJson).subscribe((response: any) => {
+      console.log("Response from post blog api", response)
+      this.blogIdCreateBlog = response.id
+      this.router.navigate(['/view-blog/' + this.blogIdCreateBlog])
+      this.loader.stop()
+    }, (error) => {
+      console.log("Error from post blog api", error)
+      this.loader.stop()
+    })
+  }
 
   ngOnInit(): void {
   }
